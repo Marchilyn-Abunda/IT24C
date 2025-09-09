c472dcca40540b3cd1ddb946399e2fd6e9905a74
Marchilyn Abunda <marchilynabunda@gmail.com>
2025-09-09 16:13:21 +0800

    Added Avtivity 2 : Basic web and DOM element

---
+++ Activity2_Basics_of_Web_and_DOM_Elements/script.js
@@ -0,0 +1,35 @@
+function updateDisplay() {
+  let textbox = document.getElementById("textinput");
+  let displaytext = document.getElementById("nameDisplay");
+
+  displaytext.textContent = textbox.value;
+}
+
+let quotes = [
+  "Everything happens for a reason",
+  "Trust God with all of your Hearts",
+  "Small progress each day adds up to big results",
+  "Your only limit is your mind",
+  "Don't stop when you're tired, stop when you're done",
+];
+
+let displayQuote = document.getElementById("quoteDisplay");
+
+function displayRandom() {
+  for (let i = 0; i < quotes.length; i++) {
+    let randomIndex = Math.floor(Math.random() * quotes.length);
+
+    displayQuote.textContent = quotes[randomIndex];
+  }
+}
+
+//   for (let i = 0; i < quotes.length; i++) {
+//     const randomIndex = Math.floor(Math.random() * quotes.length);
+
+//     document.getElementById("surprise").addEventListener("click", function(){
+//          displayQuote.textContent = quotes[randomIndex];
+//     });
+   
+//   }
+
+
